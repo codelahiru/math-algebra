42b6959ef964f6e0daf87c325795606e91669dc4
codelahiru <gayan.hatharasinghe@gmail.com>
2024-09-11 23:44:01 +0530

    Matrix - eigenvals

--- symbolic-computation/intro.py
+++ symbolic-computation/intro.py
@@ -14,8 +14,14 @@ print(y)
 t = integrate(exp(x)*sin(x) + exp(x)*cos(x), x)
 print(t)
 
-k = integrate(sin(x**2), (x, -oo, oo))
-print(k)
+k1 = integrate(sin(x**2), (x, -oo, oo))
+print(k1)
 
 k2 = limit(sin(x)/x, x, 0)
 print(k2)
+
+k3 = solve(x**2 - 2, x)
+print(k3)
+
+k4 = Matrix([[1, 2], [2, 2]]).eigenvals()
+print(k4)
